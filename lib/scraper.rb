90e8fd6fa8df4bce1a2b7dd2c185f32cd27f9e5e
Richard Sanchez <richard.charles.s@gmail.com>
2018-08-20 20:19:35 +0000

    scraper build

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,4 +4,15 @@ require 'pry'
 
 class Scraper
 
+  index_url = "https://www.cntraveler.com/gallery/best-museums-in-new-york-city"
+
+  def scrape_index_page(index_url)
+    index = Nokogiri::HTML(index_html)
+    museum = index.css(".image-content-container")
+    museums = []
+    museum.collect do |museum_content|
+    museums << {
+      :name => museum_content.css(".h2.hed").text
+      :museum_profile_url => museum_content.css.("a").attribute("href").value
+      }
 end
