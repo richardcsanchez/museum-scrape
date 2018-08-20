b725c558705158ac795919a8fe54f1d8cf9d43fc
Richard Sanchez <richard.charles.s@gmail.com>
2018-08-20 21:21:28 +0000

    updated to require all related files

--- config/evironment.rb
+++ config/evironment.rb
@@ -1,2 +1,6 @@
 require 'bundler/setup'
 Bundler.require(:default, :development)
+
+require_relative "../lib/scraper.rb"
+require_relative "../lib/museums.rb"
+require_relative "..lib/museum_scraper.rb"
