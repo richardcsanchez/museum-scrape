36655e9d22a55b9a3cf083e0933459135747d974
Richard Sanchez <richard.charles.s@gmail.com>
2018-08-20 21:24:53 +0000

    require config & psuedo-coded collect museum object&removed unnecessary museum array variable

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,6 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
 require 'pry'
+require_relative "../config/environment"
 
 class Scraper
 
@@ -8,12 +9,21 @@ class Scraper
 
   def scrape_index_page(index_url)
     index = Nokogiri::HTML(index_html)
-    museum = index.css(".image-content-container")
-    museums = []
-    museum.collect do |museum_content|
-    museums << {
-      :name => museum_content.css(".h2.hed").text
-      :museum_profile_url => museum_content.css.("a").attribute("href").value
+    scraped_museums = index.css(".image-content-container")
+    #museums = []
+    x = scraped_museums.collect do |museum_content|
+      #creating museum objects to be collected
+    {:name => museum_content.css(".h2.hed").text
+      :museum_page_url => museum_content.css.("a").attribute("href").value
       }
-      binding.pry
+    binding.pry
+  end
+
+  def self.scrape_museum_page(museum_page_url)
+    museum_html = open(profile_url)
+    museum_index = Nokogiri::HTML(museum_html)
+    museum_details = {}
+
+
+
   end
