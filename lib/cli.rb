921a48bdfe2e975e8c13cb3d618f584324821748
Richard Sanchez <richard.charles.s@gmail.com>
2018-08-21 21:51:01 +0000

    updated #run and added colorize gem

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,6 +1,7 @@
 require_relative "../lib/scraper.rb"
 require_relative "../lib/museums.rb"
 require_relative "../lib/cli.rb"
+require 'colorize'
 
 
 class CLI
@@ -9,12 +10,10 @@ class CLI
 
   def run
     make_museums #Start the scrape
-    puts "Welcome to your NYC Museum Guide!"
-    puts "Here's NYC's 25 Best Museums:"
+    puts "Welcome to your NYC Museum Guide!".bold.colorize(:yellow)
+    puts "Here's NYC's 25 Best Museums:".bold.colorize(:yellow)
       list_museums #list museum object name
-    puts "Which would you like to learn more about?"
-    
-    #ask user for input
+    museum_details
   end
 
   def make_museums
@@ -28,6 +27,16 @@ class CLI
       puts  "#{index}. #{museum.name}"
     end
   end
+
+  def museum_details
+    puts "Please enter the number of the museum you would like to learn more about".bold.colorize(:yellow)
+      input = gets.chomp.to_i
+      if (1..25).include?(input)
+        chosen_museum = Museum.all.sort_by {|museum| museum.name}[input - 1]
+        puts "#{chosen_museum.name}".bold.colorize(:blue)
+        puts "#{chosen_museum.museum_description}"
+      end
+    end
 
 
 end
