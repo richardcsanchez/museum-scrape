5285b9d22c6ffb5ef2228bde1361223427777f1c
Richard Sanchez <richard.charles.s@gmail.com>
2018-08-21 18:34:25 +0000

    updated :museum_site_url value to reflect full url

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,31 +7,32 @@ require_relative "../lib/cli.rb"
 
 class Scraper
 
-   BASE_PATH = "https://www.cntraveler.com/"
+   BASE_PATH = "https://www.cntraveler.com"
    @museum_list_url = (BASE_PATH + "/gallery/best-museums-in-new-york-city")
+   #@museum_page_url = (BASE_PATH + "#{@museum_objects_collection[1]}")
 
   def self.scrape_index_page(museum_list_url)
       doc = HTTParty.get(museum_list_url)
       index = Nokogiri::HTML(doc)
       @scraped_museums = index.css(".gallery-item.gallery-item-venue")
-      museum_objects_collection = []
+      @museum_objects_collection = []
       @scraped_museums.collect do |museum_content|
-        museum_objects_collection << {
+        @museum_objects_collection << {
           :name => museum_content.css("h2.hed").text,
-          :museum_page_url => museum_content.css("a").attribute("href").value
+          :museum_site_url => BASE_PATH + museum_content.css("a").attribute("href").value
         }
       end
-      museum_objects_collection
+      @museum_objects_collection
     end
 
 
-  def self.scrape_museum_page(museum_page_url)
-    museum_html = open(profile_url)
+  def self.scrape_museum_page(museum_site_url)
+    museum_html = HTTParty.get(museum_site_url)
     museum_index = Nokogiri::HTML(museum_html)
     museum_details = {}
     museum_index.css("div.venue-grid-wrapper.name-info").each do |museum_page_header|
       museum_details << {
-        :name => museum_page_header.css("h1")
+        :name => museum_page_header.css("h1"),
         :short_description => museum_page_header.css("div")
       }
     end
