ef22b4113e6a56f69cf7ec5a3a173cf73260a0f6
Richard Sanchez <richard.charles.s@gmail.com>
2018-08-21 21:17:26 +0000

    begin coding CLI portion of run method

--- lib/cli.rb
+++ lib/cli.rb
@@ -8,10 +8,12 @@ class CLI
   attr_accessor :museum_list_url
 
   def run
-    #Start the scrape
-    make_museums
-    list_museums
-    #list museum object name
+    make_museums #Start the scrape
+    puts "Welcome to your NYC Museum Guide!"
+    puts "Here's NYC's 25 Best Museums:"
+      list_museums #list museum object name
+    puts "Which would you like to learn more about?"
+    
     #ask user for input
   end
 
@@ -22,7 +24,7 @@ class CLI
 
   def list_museums
     museum_alphabetical = Museum.all.sort_by {|museum| museum.name}
-    museum_alphabetical.all.each.with_index(1) do |museum, index|
+    museum_alphabetical.each.with_index(1) do |museum, index|
       puts  "#{index}. #{museum.name}"
     end
   end
