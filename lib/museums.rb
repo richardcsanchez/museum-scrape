acf60133d913bc0a8cab1190b55d3614b17e28a0
Richard Sanchez <richard.charles.s@gmail.com>
2018-08-27 19:35:01 +0000

    refactored & trimmed down self.scrape_index_page method

--- lib/museums.rb
+++ lib/museums.rb
@@ -10,12 +10,13 @@ class Museum
   @@all = []
 
   def initialize(museum_collection)
+    #binding.pry
     museum_collection.each_pair {|key, value| self.send(("#{key}="), ("#{value}"))}
       @@all << self
     end
 
     def self.create_from_array(museum_array)
-      museum_array.each { |museum_hash| self.new(student_hash)}
+      museum_array.each { |museum_hash| self.new(museum_hash)}
     end
 
     def self.all
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,15 +18,13 @@ class Scraper
       doc = HTTParty.get(museum_list_url)
       index = Nokogiri::HTML(doc)
       scraped_museums = index.css(".gallery-item.gallery-item-venue")
-      museum_hash = []
       scraped_museums.collect do |museum_content|
-        museum_hash << {
+         {
           :name => museum_content.css("h2.hed").text,
           :museum_site_url => BASE_PATH + museum_content.css("a").attribute("href").value,
           :museum_description => museum_content.css("div.dek").css("p").text
         }
       end
-      museum_hash #returns collection of scraped museums with attributes
     end
 
 
