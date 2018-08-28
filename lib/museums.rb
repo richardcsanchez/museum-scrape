111fef81940b43e41a75c936338b45d4fd80efbe
Richard Sanchez <richard.charles.s@gmail.com>
2018-08-28 21:05:16 +0000

    removed pseudocode from museums.rb

--- lib/museums.rb
+++ lib/museums.rb
@@ -6,16 +6,12 @@ class Museum
 
   @@all = []
 
-  def initialize(museum_hash) # take in an argument of a hash and assigns the 
-    #new museum attributes and values to match with the key/value pairs of the hash
-    #and saves in @@all array. Send method dynamically calls on the attr_accessor methods
-    #and creates key => value pairs
+  def initialize(museum_hash)
     museum_hash.each_pair {|key, value| self.send(("#{key}="), ("#{value}"))}
       @@all << self
     end
 
     def self.create_from_array(museum_array)
-      #takes in array of hashes and iterated over them to create new individual museums
       museum_array.each { |museum_hash| self.new(museum_hash)}
     end
 
