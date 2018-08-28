c49f558fac34f7dd0648256fea2b27c4a53b66af
Richard Sanchez <richard.charles.s@gmail.com>
2018-08-28 21:05:59 +0000

    removed psuedocode from scraper.rb

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,10 @@ class Scraper
 
   attr_accessor :museum_list_url
 
-   BASE_PATH = "https://www.cntraveler.com" #original website
+   BASE_PATH = "https://www.cntraveler.com"
    museum_list_url = (BASE_PATH + "/gallery/best-museums-in-new-york-city")
-    #combines original website with specific list page BASE_PATH and list page
-    #are separated to allow museum site urls to be created
 
-  def self.scrape_index_page(museum_list_url) #scrapes museum list site
+  def self.scrape_index_page(museum_list_url) 
       doc = HTTParty.get(museum_list_url)
       index = Nokogiri::HTML(doc)
       scraped_museums = index.css(".gallery-item.gallery-item-venue")
